983dc3effedee4f45e6a430635976614b67e1506
leoven4 <leonardo.ventura424@gmail.com>
2024-07-19 12:16:09 +0200

    about me

--- src/routes/intro/intro.jsx
+++ src/routes/intro/intro.jsx
@@ -3,7 +3,24 @@ import "./intro.scss"
 const Intro = () => {
 
   return (
-    <div className="card_wrapper">
+    <div>
+      <h1 className="title">ABOUT ME</h1>
+      <div className="portrait"></div>
+      <div className="text">
+        <p>I’m architect Martina Anelli from Bologna, Italy.</p>
+        <p>From the very beginning of my career as a student first and as professional then, I always believed in the power that architecture has on the people who experience it. It influences their behavior, their habits, their sense of community and their feel of safety in places that they live every day. For these reasons in my work I always tried to put persons first. To achieve this goal is important to combine empathy with clients and creativity, not only in terms of composition but also of process and technology, with a specific attention for green solutions and techniques to create more comfortable and livable spaces. </p>
+        <p>I have a double degree in Architecture and Building Engineering from University of Bologna, in Italy. During my academic years I had the opportunity to study abroad and getting in contact with different approaches and methodologies where I discovered and take into practice my passion for landscape, interior design and sustainable architecture. Further, I joined a couple of international intensive programs, one in Bologna concerning urban regeneration and social housing, the other one in Kassel, Germany, about Landscape Education for Democracy. All these experiences had increased my interest in urban planning and gave me the instruments for real practices to use in spatial planning for a democratic grown of the city where people come first.</p>
+        <p>After my studies I worked for almost 3 years in some studios in my hometown designing especially residential buildings, new one and renovating existing buildings or flats, but also some offices, retail and commercial spaces, at different scales and at different level of design. Thanks to these works I had the chance to explore new techniques and materials connected to green building architecture and sustainability. </p>
+        <p>I am a passionate designer and a strong team player. I have really good organizational skills, able to work under pressure and meet deadlines. I also have the ability of problem-solving, I am self-motivated, curious and able to take initiative in response to direction from managers. A flexible and open attitude towards new ways of working and with a huge will to learn new approaches in architecture and interior design. I aspire to continue working in a challenging and creative environment where my skills could improve and put themselves to the test​​​​​​​.       </p>
+        <p>Furthermore, I started to work with the italian architect Cristina Marchi  specialized in green building constructions. Here I learnt how to design  with sustainable techniques, I had relations with suppliers and I improved my skills in interior design, trying  to combine contemporary design with raw and natural materials. </p>
+        <p>I greatly admire your commitment to environmentally conscious architecture and your innovative approach to creating spaces that harmonize with their surroundings, or not. I cannot forget the first time I visited Fondazione Prada in Milan and the impact that this project had on me as  visitor and as architect. It would be an incredible opportunity to contribute to your firm's vision and design.</p>
+        <p>I am particularly motivated to work in Rotterdam due to its vibrant architecture, rich cultural heritage, and progressive urban development. I am excited about the prospect of immersing myself in the city's dynamic architectural scene and contributing to its evolving skyline.</p>
+        <p>Enclosed is my resume together with my portfolio in A4 format, which provides further details about my academic background, practical experience, and skills. I would welcome the opportunity to discuss how my qualifications align with the needs of the company in more detail. </p>      
+        <p>Thank you for considering my application.</p>
+        <p>I look forward to the possibility of joining your team and contributing to your firm's continued success. Thank you for your time and consideration.</p>
+        <p>Sincerely,</p>
+        <p>Martina Anelli</p>
+      </div>
     </div>
     );
 
