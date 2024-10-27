4ba7d080a8dc22fb9c220354492877b9317b512f
leoven4 <leonardo.ventura424@gmail.com>
2024-10-27 10:12:34 +0100

    gallery data added

--- src/routes/gallery/gallery_data.jsx
+++ src/routes/gallery/gallery_data.jsx
@@ -1,5 +1,88 @@
 const gallery_content = {
 
+    casa_martina : {
+        name: "casa martina",
+        url: "",
+        year: "",
+    },
+
+    casa_federico : {
+        name: "casa federico",
+        url: "",
+        year: "",
+    },
+
+    casa_andrea : {
+        name: "casa andrea",
+        url: "",
+        year: "",
+    },
+
+    terrazza_nancy : {
+        name: "terrazza nancy",
+        url: "",
+        year: "",
+    },
+
+    torre : {
+        name: "la torre",
+        imageUrl: "https://drive.google.com/thumbnail?id=1TQSZGXMeesOJFabQiTeWbOMh5jlnFiJq&sz=w1000",
+        year: '2022'},
+
+    wellness_center : {
+        name: "water and roses welness center",
+        url: "",
+        year: "",
+    },
+
+    skyclimber_ticinum : {
+        name: "skyclimber ticinum",
+        url: "",
+        year: "",
+    },
+
+    garden_ball : {
+        name: "garden ball",
+        url: "",
+        year: "",
+    },
+
+    muba : {
+        name: "muba",
+        url: "",
+        year: "",
+    },
+
+    porticoes : {
+        name: "porticoes of bologna for unesco",
+        url: "",
+        year: "",
+    },
+
+    fair_booths : {
+        name: "fair booths",
+        url: "",
+        year: "",
+    },
+
+    nordstadt : {
+        name: "nordstadt: join the colors",
+        url: "",
+        year: "",
+    },
+
+    multiple_city : {
+        name: "multiple-city",
+        url: "",
+        year: "",
+    },
+
+    sava : {
+        name: "the sava valley",
+        url: "",
+        year: "",
+    },
+
     guardia25 : {
         name: "guardia25",
         imageUrl: "https://drive.google.com/thumbnail?id=1y8hkTteX1zzN_-EpZhqnedDbPe-h3yEj&sz=w1000",
@@ -10,9 +93,5 @@ const gallery_content = {
         imageUrl: "https://drive.google.com/thumbnail?id=1R2qrX7EJooTv-PrjujpwsAF-zaqV8GLr&sz=w1000",
         year: '2023'},
 
-    torre : {
-        name: "la torre",
-        imageUrl: "https://drive.google.com/thumbnail?id=1TQSZGXMeesOJFabQiTeWbOMh5jlnFiJq&sz=w1000",
-        year: '2022'},
 
 }
